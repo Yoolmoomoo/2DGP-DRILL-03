0eb0a3bd6fef492f672677f96f32ba7ec4392c76
yoolmoo(Lenovo) <dev.juho.y@gmail.com>
2024-09-24 10:20:08 +0900

    run_circle 완성

--- character_grass.py
+++ character_grass.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 open_canvas()
 
@@ -10,8 +11,17 @@ def run_rectangle():
     pass    # pass : 아직 생각이 잘 안돼서 일단 비워놓는 키워드
 
 def run_circle():
-    boy.draw_now(400,90)
-    delay(1)
+        #Logic
+    r, cx, cy = 300, 800 // 2, 600 // 2
+    for degree in range(0, 360):
+        x = r * math.cos(math.radians(degree)) + cx
+        y = r * math.sin(math.radians(degree)) + cy
+
+        #Rendering
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        boy.draw_now(x,y)
+        delay(0.01)
     pass
 
 while True:
