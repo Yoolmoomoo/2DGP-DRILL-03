8fba905610a3dcf52ba520fc3e8aad776a412336
yoolmoo(Lenovo) <dev.juho.y@gmail.com>
2024-09-24 10:52:42 +0900

    run_rectangle 완성

--- character_grass.py
+++ character_grass.py
@@ -17,11 +17,16 @@ def run_top():
         draw_boy(x, 550)
     pass
 def run_right():
-    print('RIGHT')
+    for y in range(550, -10, -10):
+        draw_boy(790, y)
     pass
 def run_bottom():
+    for x in range(790, -10, -10):
+        draw_boy(x, 0)
     pass
 def run_left():
+    for y in range(0, 560, 10):
+        draw_boy(0, y)
     pass
 
 def run_rectangle():
@@ -43,8 +48,14 @@ def run_circle():
     pass
 
 while True:
-    # run_circle()
+    run_circle()
     run_rectangle()
-    break
+    
 
 close_canvas()
+
+# 첫 줄 시작을 잘해야 한다. 곧바로 디테일하게 들어가지 말고 먼저 뼈대부터 잡는다. 함수를 먼저 호출하고 정의는 나중에 한다.
+
+# 커밋은 그때그때 실행가능한 파일을 기준으로 하자.
+
+# 테스트 리드 타임을 줄이기 위해 이미 검증된 코드는 실행에서 제외시킨다.
