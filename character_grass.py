7be44d75e15373a5bec70d584a98ee8372c62532
yoolmoo(Lenovo) <dev.juho.y@gmail.com>
2024-09-24 09:53:35 +0900

    프린트 통해서 루프 정상 실행 확인

--- character_grass.py
+++ character_grass.py
@@ -1,9 +1,11 @@
 from pico2d import *
 
 def run_rectangle():
+    print('RECTANGLE')
     pass    # pass : 아직 생각이 잘 안돼서 일단 비워놓는 키워드
 
 def run_circle():
+    print('CIRCLE')
     pass
 
 open_canvas()
@@ -12,10 +14,8 @@ open_canvas()
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
-type(grass)
-
-# while True:
-#     run_rectangle()
-#     run_circle()
+while True:
+    run_circle()
+    run_rectangle()
 
 close_canvas()
