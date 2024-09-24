4496658a4cf1c933ca2c977812febce251e98ec9
yoolmoo(Lenovo) <dev.juho.y@gmail.com>
2024-09-24 10:02:29 +0900

    run_circle 캐릭터 출력

--- character_grass.py
+++ character_grass.py
@@ -1,21 +1,22 @@
 from pico2d import *
 
-def run_rectangle():
-    print('RECTANGLE')
-    pass    # pass : 아직 생각이 잘 안돼서 일단 비워놓는 키워드
-
-def run_circle():
-    print('CIRCLE')
-    pass
-
 open_canvas()
 
 # fill here
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
+def run_rectangle():
+    pass    # pass : 아직 생각이 잘 안돼서 일단 비워놓는 키워드
+
+def run_circle():
+    boy.draw_now(400,90)
+    delay(1)
+    pass
+
 while True:
     run_circle()
     run_rectangle()
+    break
 
 close_canvas()
