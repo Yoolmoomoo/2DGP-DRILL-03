1bbecb1b7a14a28e0ca8097c4a1d4a6ce62cdfb1
yoolmoo(Lenovo) <dev.juho.y@gmail.com>
2024-09-24 09:49:56 +0900

    전체적인 구조 완성
    - run_rectangle, run_circle

--- character_grass.py
+++ character_grass.py
@@ -1,7 +1,21 @@
 from pico2d import *
 
+def run_rectangle():
+    pass    # pass : 아직 생각이 잘 안돼서 일단 비워놓는 키워드
+
+def run_circle():
+    pass
+
 open_canvas()
 
 # fill here
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
+type(grass)
+
+# while True:
+#     run_rectangle()
+#     run_circle()
 
 close_canvas()
